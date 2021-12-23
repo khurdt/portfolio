b7181883a9d6d8b9da1d9682887406766b14d8a9
Kevin Hurdt <93124068+khurdt@users.noreply.github.com>
2021-12-23 16:55:46 -0600

    added validation functions to contact page

--- js/script.js
+++ js/script.js
@@ -1,34 +1,107 @@
-//selecting HTML elements
-let emailElement = document.querySelector('#email');
-let messageElement = document.querySelector('#message')
-let submitButton = document.querySelector('#submit-button')
-submitButton.addEventListener('click', function(e) {
-	e.preventDefault();
-	console.log('clicked button')
+(function() {
+  let form = document.querySelector('#register-form'),
+      emailInput = document.querySelector('#email'),
+      passwordInput = document.querySelector('#password'),
+      firstNameInput = document.querySelector('#first-name'),
+      lastNameInput = document.querySelector('#last-name');
+      phoneInput = document.querySelector('#phone');
 
-	let emailValue = emailElement.value;
-	let messageValue = messageElement.value;
+    function showErrorMessage(input, message) {
+    let container = input.parentElement;
 
-	console.log('Email: ', emailValue)
-	console.log('Message: ', messageValue )
+    let error = container.querySelector('.error-message');
 
-	if(emailValue.includes('@')) {
-		//all good
-		alert('thank you for your message')
-	} else {
-		alert('I am sorry, that was not a correct email address, please try again');
-		}
-
-	function addSpaces(initial){
-        initial.replace("[0-9]{3}[d-][0-9]{3}");
-        initial.replace("[0-9]{3}[d-][0-9]{3}[d-][0-9]{4}");
-        return initial;
+    if(error) {
+      container.removeChild(error);
     }
-})
 
+    if (message) {
+      let error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  };
 
+  function validateFirstName() {
+    let value = firstNameInput.value;
 
+    if(!value) {
+      showErrorMessage(firstNameInput, '*First name is required')
+      return false;
+    };
 
-//attaching "quick listener"
-//getting user entered values
-//javascript validations
+    showErrorMessage(firstNameInput, null);
+    return true;
+  };
+
+  function validateLastName() {
+    let value = lastNameInput.value;
+
+    if(!value) {
+      showErrorMessage(lastNameInput, '*Last name is required')
+      return false;
+    };
+
+    showErrorMessage(lastNameInput, null);
+    return true;
+  };
+
+  function validateEmail() {
+    let value = emailInput.value;
+
+    if (!value) {
+      showErrorMessage(emailInput, '*Email is a required field');
+      return false;
+    };
+
+    if (value.indexOf('@') === -1) {
+      showErrorMessage(emailInput, '*Enter a valid email');
+      return false;
+    };
+
+    showErrorMessage(emailInput, null);
+    return true;
+  };
+
+  function validatePhone() {
+    let value = phoneInput.value;
+
+    if (value.length !== 10) {
+      showErrorMessage(phoneInput, '*Enter a valid phone number');
+      return false;
+    };
+
+      showErrorMessage(phoneInput, null);
+      return true;
+  };
+
+  function addDashes() {
+    let phone = phoneInput.value
+    phone = phone.split('-').join('');
+    phone = (phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'));
+    console.log(phone);
+  };
+
+  function validateForm() {
+   let goodEmail = validateEmail(),
+       goodFirstName = validateFirstName(),
+       goodLastName = validateLastName(),
+       goodPhone = validatePhone();
+    return goodEmail && goodFirstName && goodLastName && goodPhone;
+  };
+//Event Listeners
+	form.addEventListener('submit', (event) => {
+	    event.preventDefault(); // Do not submit to the server
+	    if (validateForm()) {
+	      alert('Success!');
+	    }
+	  })
+
+	emailInput.addEventListener('input', validateEmail);
+	firstNameInput.addEventListener('input', validateFirstName);
+  lastNameInput.addEventListener('input', validateLastName);
+  phoneInput.addEventListener('input', validatePhone);
+  phoneInput.addEventListener('keyup', addDashes);
+
+})();
