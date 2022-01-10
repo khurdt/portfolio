4bd4bd60628f08d854f807cbe562376776156f46
Kevin Hurdt <93124068+khurdt@users.noreply.github.com>
2022-01-10 12:37:24 -0600

    added eslint and removed files

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 (function() {
   let form = document.querySelector('#register-form'),
       emailInput = document.querySelector('#email'),
-      passwordInput = document.querySelector('#password'),
       firstNameInput = document.querySelector('#first-name'),
-      lastNameInput = document.querySelector('#last-name');
+      lastNameInput = document.querySelector('#last-name'),
       phoneInput = document.querySelector('#phone');
 
     function showErrorMessage(input, message) {
@@ -21,7 +20,7 @@
       error.innerText = message;
       container.appendChild(error);
     }
-  };
+  }
 
   function validateFirstName() {
     let value = firstNameInput.value;
@@ -29,11 +28,11 @@
     if(!value) {
       showErrorMessage(firstNameInput, '*First name is required')
       return false;
-    };
+    }
 
     showErrorMessage(firstNameInput, null);
     return true;
-  };
+  }
 
   function validateLastName() {
     let value = lastNameInput.value;
@@ -41,11 +40,11 @@
     if(!value) {
       showErrorMessage(lastNameInput, '*Last name is required')
       return false;
-    };
+    }
 
     showErrorMessage(lastNameInput, null);
     return true;
-  };
+  }
 
   function validateEmail() {
     let value = emailInput.value;
@@ -53,16 +52,16 @@
     if (!value) {
       showErrorMessage(emailInput, '*Email is a required field');
       return false;
-    };
+    }
 
     if (value.indexOf('@') === -1) {
       showErrorMessage(emailInput, '*Enter a valid email');
       return false;
-    };
+    }
 
     showErrorMessage(emailInput, null);
     return true;
-  };
+  }
 
   function validatePhone() {
     let value = phoneInput.value;
@@ -70,18 +69,18 @@
     if (value.length !== 9) {
       showErrorMessage(phoneInput, '*Enter a valid phone number');
       return false;
-    };
+    }
 
       showErrorMessage(phoneInput, null);
       return true;
-  };
+  }
 
   function addDashes() {
     let phone = phoneInput.value
     phone = phone.split('-').join('');
     phone = (phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'));
     console.log(phone);
-  };
+  }
 
   function validateForm() {
    let goodEmail = validateEmail(),
@@ -89,14 +88,14 @@
        goodLastName = validateLastName(),
        goodPhone = validatePhone();
     return goodEmail && goodFirstName && goodLastName && goodPhone;
-  };
+  }
 //Event Listeners
 	form.addEventListener('submit', (event) => {
-	    event.preventDefault(); // Do not submit to the server
-	    if (validateForm()) {
-	      alert('Success!');
-	    }
-	  })
+    event.preventDefault(); // Do not submit to the server
+    if (validateForm()) {
+      alert('Success!');
+    }
+  })
 
 	emailInput.addEventListener('input', validateEmail);
 	firstNameInput.addEventListener('input', validateFirstName);
