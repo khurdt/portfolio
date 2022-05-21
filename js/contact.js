7a08d441bdba28acd65631a9590c8e039f9334f8
khurdt <joshuahurdt@gmail.com>
2022-05-21 04:45:13 -0500

    update cors

--- js/contact.js
+++ js/contact.js
@@ -128,8 +128,8 @@ function submit(e) {
   let xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://th4ow5edkf.execute-api.us-east-1.amazonaws.com/dev/', true);
 
+  xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://khurdt.github.io/portfolio/contact.html');
   xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
   xhr.onreadystatechange = function () {
     if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
     }
