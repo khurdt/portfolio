3b35d852a0ed4337a082c0fdf2addd6da420e296
khurdt <joshuahurdt@gmail.com>
2022-05-21 03:15:57 -0500

    updated cors policy

--- handler.js
+++ handler.js
@@ -14,8 +14,22 @@ if (!config.region) {
 
 const ses = new SES();
 
+let allowedOrigins = [
+  'https://khurdt.github.io',
+  'https://khurdt.github.io/portfolio/contact.html'
+];
+
+//implementing limits using CORS
 app.use(cors({
-  origin: 'https://khurdt.github.io/portfolio/contact.html'
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      //If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 app.use(urlencoded({ extended: false }));
